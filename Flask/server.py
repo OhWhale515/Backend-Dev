50ca40a565dccf1489b9512210a444d4ddf8ca9f
OhWhale515 <sterlinggreen515@gmail.com>
2022-12-26 17:52:57 -0500

    created index.html and app.route

--- Flask/server.py
+++ Flask/server.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
@@ -8,13 +9,13 @@ def hello_world():
             '<p>This is the paragraph.</p>' \
             '<img src="https://media.giphy.com/media/3oEdv2qNBprY4gDxMk/giphy.gif" width="200>'
 
-@app.route("/bye")
-def say_bye():
-    return "Bye!"
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-@app.route("/username/<name>/<int:number>")
-def greet(name, number):
-    return f"Hello there {name}!, you are {number} years old."
+# @app.route("/username/<name>/<int:number>")
+# def greet(name, number):
+#     return f"Hello there {name}!, you are {number} years old."
 
 if __name__ == "__main__":
     app.run()
