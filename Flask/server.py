2e4899782eef75d324eb3d2873b95ed9cd0d9df7
OhWhale515 <sterlinggreen515@gmail.com>
2022-12-26 18:01:58 -0500

    added random app

--- Flask/server.py
+++ Flask/server.py
@@ -11,11 +11,10 @@ def hello_world():
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    random_number = random.randint(1, 10)
+    return render_template("index.html", num=random_number)
 
-# @app.route("/username/<name>/<int:number>")
-# def greet(name, number):
-#     return f"Hello there {name}!, you are {number} years old."
+
 
 if __name__ == "__main__":
     app.run()
